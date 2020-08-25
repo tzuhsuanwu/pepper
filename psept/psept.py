f16395584a5f7010b8bc2208fec7f29c2118f5fa
KODWu <kod826363@gmail.com>
2020-08-25 22:34:35 +0800

    feat (Psept): Add mutate function
    feat (Psept): Add global variable:
    	__wt_columns, __mu_columns, __diff_columns, __whole_columns
    feat (Psept): Add pyrosetta_scoring function
    feat (Psept.set_pdb_folder): Add set full and clean pdb foler
    refactor (Psept.create_ordered_pdb_list): delete wt_aa and mu_aa

--- psept/psept.py
+++ psept/psept.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import pandas as pd
+import numpy as np
 import json
 import re
 import os
@@ -8,15 +9,39 @@ from urllib.error import HTTPError
 from pyrosetta import init
 from pyrosetta.io import pose_from_pdb
 from pyrosetta.toolbox.cleaning import cleanATOM
+from pyrosetta.toolbox.mutants import mutate_residue
+from pyrosetta.rosetta.core.scoring import ScoreFunction
+from pyrosetta.rosetta.core.scoring import ScoreType
+from pyrosetta.rosetta.core.scoring import calc_total_sasa
 
 
 class Psept:
     __repack_radius = 8
     __probe_radius = 2.2
 
+    __wt_columns = [
+        "wt_fa_atr", "wt_fa_rep", "wt_fa_intra_rep", "wt_fa_sol", "wt_lk_ball_wtd", "wt_fa_intra_sol_xover4",
+        "wt_fa_elec", "wt_hbond_lr_bb", "wt_hbond_sr_bb", "wt_hbond_bb_sc", "wt_hbond_sc", "wt_dslf_fa13",
+        "wt_rama_prepro", "wt_p_aa_pp", "wt_fa_dun", "wt_omega", "wt_pro_close", "wt_yhh_planarity", "wt_ref", "wt_sum"
+    ]
+
+    __mu_columns = [
+        "mu_fa_atr", "mu_fa_rep", "mu_fa_intra_rep", "mu_fa_sol", "mu_lk_ball_wtd", "mu_fa_intra_sol_xover4",
+        "mu_fa_elec", "mu_hbond_lr_bb", "mu_hbond_sr_bb", "mu_hbond_bb_sc", "mu_hbond_sc", "mu_dslf_fa13",
+        "mu_rama_prepro", "mu_p_aa_pp", "mu_fa_dun", "mu_omega", "mu_pro_close", "mu_yhh_planarity", "mu_ref", "mu_sum"
+    ]
+
+    __diff_columns = [
+        "diff_fa_atr", "diff_fa_rep", "diff_fa_intra_rep", "diff_fa_sol", "diff_lk_ball_wtd", "diff_fa_intra_sol_xover4",
+        "diff_fa_elec", "diff_hbond_lr_bb", "diff_hbond_sr_bb", "diff_hbond_bb_sc", "diff_hbond_sc", "diff_dslf_fa13",
+        "diff_rama_prepro", "diff_p_aa_pp", "diff_fa_dun", "diff_omega", "diff_pro_close", "diff_yhh_planarity", "diff_ref", "diff_sum"
+    ]
+
+    __whole_column = __wt_columns + __mu_columns + __diff_columns
+
     def __init__(self, pdb_info_path='pdb_info.json'):
         """
-        Initilize and setting for Psept.
+        Initilize and setting for Psept
         """
         self.__data = None
 
@@ -31,12 +56,14 @@ class Psept:
                 'load_failed': {'source': '', 'date': '', 'data': ''},
                 'multimodel': {'source': '', 'date': '', 'data': ''}
             }
-        self.__pdb_root_folder = Path('./pdb_files')
+        self.__root_pdb_folder = Path('./pdb_files')
+        self.__full_pdb_folder = self.__root_pdb_folder / 'full_pdb'
+        self.__clean_pdb_folder = self.__root_pdb_folder / 'clean_pdb'
         self.__skip_pdb_list = self.__pdb_info['download_failed']['data'] + self.__pdb_info['load_failed']['data'] + self.__pdb_info['multimodel']['data']
 
     def load_savs(self, filepath):
         """
-        Load input sav file. 
+        Load input sav file.
         Input format: {UniProt ID} {mutated position} {wildtype amino acid} {mutated amino acid}
         e.g., P04637 35 L F
         """
@@ -49,8 +76,8 @@ class Psept:
 
     def show_data(self, datatype='list'):
         """
-        Show loaded input data.
-        Defalut type is list. DataFrame is available.
+        Show loaded input data
+        Defalut type is list. DataFrame is available
         """
         if not self.__data:
             print('No data were loaded.')
@@ -71,13 +98,15 @@ class Psept:
         return self.__pdb_info
 
     def set_pdb_folder(self, folder_path):
-        self.__pdb_root_folder = Path(folder_path)
+        self.__root_pdb_folder = Path(folder_path)
+        self.__full_pdb_folder = self.__root_pdb_folder / 'full_pdb'
+        self.__clean_pdb_folder = self.__root_pdb_folder / 'clean_pdb'
 
     @classmethod
     def download_pdb(cls, pdb):
         """
-        Download PDB file from RCSB.
-        Input: PDB code.
+        Download PDB file from RCSB
+        Input: PDB code
         """
         root_folder = Path('pdb_files')
         pdb_folder = root_folder / 'full_pdb'
@@ -94,7 +123,7 @@ class Psept:
     @classmethod
     def check_pdb_is_multimodel(cls, pdb_path):
         """
-        Check input PDB is multimodel or not.
+        Check input PDB is multimodel or not
         """
         with open(pdb_path) as f:
             if "NUMMDL" in f.read():
@@ -142,10 +171,10 @@ class Psept:
             4. Load PDB using Rosetta
                 if failed, return None
             5. Return size of PDB
-        All failed will be recorded in pdb_info.json and remove corresponding PDB file (either full or clean).
+        All failed will be recorded in pdb_info.json and remove corresponding PDB file (either full or clean)
         """
-        full_pdb_path = self.__pdb_root_folder / 'full_pdb' / (pdb + '.pdb')
-        clean_pdb_path = self.__pdb_root_folder / 'clean_pdb' / (pdb + '.clean.pdb')
+        full_pdb_path = self.__root_pdb_folder / 'full_pdb' / (pdb + '.pdb')
+        clean_pdb_path = self.__root_pdb_folder / 'clean_pdb' / (pdb + '.clean.pdb')
         if not os.path.isfile(clean_pdb_path):
             try:
                 self.download_pdb(pdb)
@@ -175,7 +204,7 @@ class Psept:
         Create ordered pdb list
         """
         for sav in self.__data:
-            uniprot, pos, wt_aa, mu_aa = sav.split()
+            uniprot, pos = sav.split()[:2]
             pos = int(pos)
             pdb_size_list = []
 
@@ -233,8 +262,147 @@ class Psept:
                 median = len(sort_by_size)//2
                 middle_start_list.append(sort_by_size.pop(median))
             self.__sav_to_ordered_pdb.append([sav, middle_start_list])
-            print(self.__sav_to_ordered_pdb)
-            break
+
+    def mutate(self):
+        """
+        Mutate each SAVs of input file
+        """
+
+        # Initialize and create result dataframe
+        result_df_column = [
+            'SAV', 'ordered_PDB_list', 'ordered_list_length', 'predict_status',
+            'current_PDB', 'current_PDB_info', 'chain', 'position', 'PDB_size', 'chain_length',
+        ]
+        result_df_column = result_df_column + self.__whole_column + ['wt_SASA', 'mu_SASA', 'diff_SASA']
+        result_df = pd.DataFrame(columns=result_df_column)
+        result_df['SAV'] = [i[0] for i in self.__sav_to_ordered_pdb]
+        result_df['ordered_PDB_list'] = [i[1] for i in self.__sav_to_ordered_pdb]
+
+        while True:
+            # Record "failed" if no more PDB in candidate list
+            result_df.loc[
+                (result_df['ordered_list_length'] == 0) & (result_df['predict_status'] == 'unfinished'),
+                'predict_status'
+            ] = 'failed'
+
+            # Extract sub_df of unfinished SAVs
+            sub_df = result_df[~result_df.predict_status.isin(['success', 'failed'])]
+
+            if sub_df.empty:
+                result_df.to_csv("result.csv", index=False)
+                break
+
+            # Pop new PDB from ordered_PDB_list and update the information
+            sub_df['current_PDB_info'] = sub_df['ordered_PDB_list'].map(lambda x: x[0])
+            sub_df['current_PDB'] = sub_df['current_PDB_info'].map(lambda x: x[0])
+            sub_df['PDB_size'] = sub_df['current_PDB_info'].map(lambda x: x[1])
+            sub_df['chain'] = sub_df['current_PDB_info'].map(lambda x: x[2])
+            sub_df['position'] = sub_df['current_PDB_info'].map(lambda x: x[3])
+            sub_df['ordered_PDB_list'] = sub_df['ordered_PDB_list'].apply(lambda x: x[1:])
+            sub_df['ordered_list_length'] = sub_df['ordered_PDB_list'].apply(lambda x: len(x))
+
+            for pdb in sub_df['current_PDB'].unique():
+
+                # Calculate wildtype PDB score
+                wt_pose = pose_from_pdb(str(self.__clean_pdb_folder / (pdb + '.clean.pdb')))
+                wt_score = np.array(self.pyrosetta_scoring(wt_pose))
+                wt_sum = np.sum(wt_score)
+                wt_score = np.append(wt_score, wt_sum)
+
+                temp_df = sub_df[sub_df['current_PDB'] == pdb]
+                for idx, row in temp_df.iterrows():
+                    chain = row['chain']
+                    position_in_pdb = row['position']
+                    wt_aa, mu_aa = row['SAV'].split()[2:4]
+
+                    # Get actual mutate position in pose
+                    position_in_pose = wt_pose.pdb_info().pdb2pose(chain, int(position_in_pdb))
+
+                    # If position not found or residue in pose not equal to SAV, continue
+                    if position_in_pose == 0 or wt_pose.residue(position_in_pose).name1() != wt_aa:
+                        sub_df.set_value(idx, 'predict_status', 'unfinished')
+                        continue
+
+                    # Get chain number and chain length
+                    chain_num = wt_pose.chain(position_in_pose)
+                    chain_length = len(wt_pose.chain_sequence(chain_num))
+
+                    # Mutate pose
+                    mutant_pose = wt_pose.clone()
+                    mutate_residue(mutant_pose, position_in_pose, mu_aa, self.__repack_radius)
+
+                    # Calculate mutant PDB score
+                    mu_score = np.array(self.pyrosetta_scoring(mutant_pose))
+                    mu_sum = np.sum(mu_score)
+                    mu_score = np.append(mu_score, mu_sum)
+
+                    # Calculate difference score
+                    diff_score = mu_score - wt_score
+
+                    result = wt_score.tolist() + mu_score.tolist() + diff_score.tolist()
+
+                    # Successful score, update the predict_status
+                    sub_df.set_value(idx, 'predict_status', 'success')
+                    sub_df.set_value(idx, 'chain_length', chain_length)
+
+                    # Calculate score of SASA
+                    wt_sasa = calc_total_sasa(wt_pose, self.__probe_radius)
+                    mu_sasa = calc_total_sasa(mutant_pose, self.__probe_radius)
+                    sub_df.set_value(idx, "wt_SASA", wt_sasa)
+                    sub_df.set_value(idx, "mu_SASA", mu_sasa)
+                    sub_df.set_value(idx, "diff_SASA", mu_sasa - wt_sasa)
+
+                    for i in range(len(self.__whole_column)):
+                        sub_df.set_value(idx, self.__whole_column[i], result[i])
+                result_df.update(sub_df)
+
+    def pyrosetta_scoring(self, pose):
+        """
+        Scoring a pose by REF2015
+        """
+        scorefxn = ScoreFunction()
+
+        scorefxn.set_weight(ScoreType.fa_atr, 1.0)
+        scorefxn.set_weight(ScoreType.fa_rep, 0.55)
+        scorefxn.set_weight(ScoreType.fa_intra_rep, 0.005)
+        scorefxn.set_weight(ScoreType.fa_sol, 1.0)
+        scorefxn.set_weight(ScoreType.lk_ball_wtd, 1.0)
+        scorefxn.set_weight(ScoreType.fa_intra_sol_xover4, 1.0)
+        scorefxn.set_weight(ScoreType.fa_elec, 1.0)
+        scorefxn.set_weight(ScoreType.hbond_lr_bb, 1.0)
+        scorefxn.set_weight(ScoreType.hbond_sr_bb, 1.0)
+        scorefxn.set_weight(ScoreType.hbond_bb_sc, 1.0)
+        scorefxn.set_weight(ScoreType.hbond_sc, 1.0)
+        scorefxn.set_weight(ScoreType.dslf_fa13, 1.25)
+        scorefxn.set_weight(ScoreType.rama_prepro, 0.45)
+        scorefxn.set_weight(ScoreType.p_aa_pp, 0.4)
+        scorefxn.set_weight(ScoreType.fa_dun, 0.7)
+        scorefxn.set_weight(ScoreType.omega, 0.6)
+        scorefxn.set_weight(ScoreType.pro_close, 1.25)
+        scorefxn.set_weight(ScoreType.yhh_planarity, 0.625)
+        scorefxn.set_weight(ScoreType.ref, 1.0)
+
+        return [
+            scorefxn.score_by_scoretype(pose, ScoreType.fa_atr),
+            scorefxn.score_by_scoretype(pose, ScoreType.fa_rep),
+            scorefxn.score_by_scoretype(pose, ScoreType.fa_intra_rep),
+            scorefxn.score_by_scoretype(pose, ScoreType.fa_sol),
+            scorefxn.score_by_scoretype(pose, ScoreType.lk_ball_wtd),
+            scorefxn.score_by_scoretype(pose, ScoreType.fa_intra_sol_xover4),
+            scorefxn.score_by_scoretype(pose, ScoreType.fa_elec),
+            scorefxn.score_by_scoretype(pose, ScoreType.hbond_lr_bb),
+            scorefxn.score_by_scoretype(pose, ScoreType.hbond_sr_bb),
+            scorefxn.score_by_scoretype(pose, ScoreType.hbond_bb_sc),
+            scorefxn.score_by_scoretype(pose, ScoreType.hbond_sc),
+            scorefxn.score_by_scoretype(pose, ScoreType.dslf_fa13),
+            scorefxn.score_by_scoretype(pose, ScoreType.rama_prepro),
+            scorefxn.score_by_scoretype(pose, ScoreType.p_aa_pp),
+            scorefxn.score_by_scoretype(pose, ScoreType.fa_dun),
+            scorefxn.score_by_scoretype(pose, ScoreType.omega),
+            scorefxn.score_by_scoretype(pose, ScoreType.pro_close),
+            scorefxn.score_by_scoretype(pose, ScoreType.yhh_planarity),
+            scorefxn.score_by_scoretype(pose, ScoreType.ref)
+        ]
 
     def scoring(self):
         """
@@ -248,6 +416,7 @@ class Psept:
 
         # Generate ordered pdb list for input savs.
         self.create_ordered_pdb_list()
+        self.mutate()
 
 
 def main():
